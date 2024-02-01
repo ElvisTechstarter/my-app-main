bf86eb32e15375b1bdd3ad6f108ee6964ce18d89
Elvis Imsirovic <elvis.imsirovic@t-online.de>
2024-02-01 23:41:28 +0100

    code fixed

--- src/components/layout/content/userinfo/UserInfo.js
+++ src/components/layout/content/userinfo/UserInfo.js
@@ -4,7 +4,8 @@ import styles from "./UserInfo.module.css";
 function UserInfo() {
   return (
     <div className={styles.mainContainer}>
-      <div className={styles.table} src={AppData.name} alt="" />
+      <div className={styles.table} />
+      {AppData.name}
     </div>
   );
 }
