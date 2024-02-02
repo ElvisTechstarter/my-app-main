1ee2ee662f2b8f4bb1de47280c7b74b6cf13320e
Elvis Imsirovic <elvis.imsirovic@t-online.de>
2024-02-02 17:32:55 +0100

    update css

--- src/components/layout/navbar/navbar-left/NavBarLeft.js
+++ src/components/layout/navbar/navbar-left/NavBarLeft.js
@@ -4,7 +4,11 @@ import styles from "./NavBarLeft.module.css";
 function NavBarLeft() {
   return (
     <div className={styles.mainContainer}>
-      <img className={styles.logImg} src={AppData.logo} alt="logo" />
+      <img
+        className={styles.logImg}
+        src="https://cdn.pixabay.com/photo/2017/01/31/23/42/animal-2028258_640.png"
+        alt="logo"
+      />
     </div>
   );
 }
