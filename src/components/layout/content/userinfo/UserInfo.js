149c14169065e34def2c22917fc925686d0dcb21
Elvis Imsirovic <elvis.imsirovic@t-online.de>
2024-02-02 17:25:21 +0100

    updated syntax

--- src/components/layout/content/userinfo/UserInfo.js
+++ src/components/layout/content/userinfo/UserInfo.js
@@ -1,17 +1,30 @@
+import styles from "./UserInfo.module.css";
 import DatabaseResponse from "../../../common/props/DatabaseResponse";
 
 function UserInfo() {
   return (
     <div>
-      <div>
-        Name: {DatabaseResponse.profile.name}
-        Age: {DatabaseResponse.profile.age}
-        City: {DatabaseResponse.profile.address}
+      <div className={styles.info}>
+        <div style={{ padding: "5px" }}>
+          Name: {DatabaseResponse.profile.name}
+        </div>
+        <div style={{ padding: "5px" }}>
+          Age: {DatabaseResponse.profile.age}
+        </div>
+        <div style={{ padding: "5px" }}>
+          City: {DatabaseResponse.profile.adress}
+        </div>
       </div>
-      <div>
-        Follower Count: {DatabaseResponse.profile.followerCount}
-        Following Count: {DatabaseResponse.profile.followingCount}
-        Post Count: {DatabaseResponse.profile.postCount}
+      <div className={styles.statisctics}>
+        <div style={{ padding: "5px" }}>
+          Follower Count: {DatabaseResponse.profile.followerCount}
+        </div>
+        <div style={{ padding: "5px" }}>
+          Following Count: {DatabaseResponse.profile.followingCount}
+        </div>
+        <div style={{ padding: "5px" }}>
+          Post Count: {DatabaseResponse.profile.postCount}
+        </div>
       </div>
     </div>
   );
